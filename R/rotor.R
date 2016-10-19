881c786062fdff82d86629dcea3b5b57770202ff
wfondrie <wfondrie@umaryland.edu>
2016-10-18 21:24:40 -0400

    added to constructor function.

--- R/rotor.R
+++ R/rotor.R
@@ -1,5 +1,41 @@
 #' Rotor class constructors
 
-rotor <- function(type = "FA", maxRadius, minRadius, sedLength, model = "custom") {
-
+rotor <- function(model = "custom",
+                  type = "SW",
+                  R.min,
+                  R.max,
+                  FA.angle,
+                  FA.diameter) {
+    
+    # Parameter checking -------------------------------------------------------
+    
+    # Custom Rotor -------------------------------------------------------------
+    if(model == "custom") {
+        rotor <- structure(list(model = model,
+                                type = type,
+                                R.min = R.min,
+                                R.max = R.max,
+                                FA.angle = FA.angle,
+                                FA.diameter = FA.diameter),
+                           class = "rotor")
+    }
+    
+    # Preset Rotors ------------------------------------------------------------
+    
+    # Swinging Bucket Rotors
+    
+    ## Beckman SW 41 Ti
+    if(model == "SW 41") {
+        rotor <- structure(list(model = "SW 41",
+                                type = "SW",
+                                R.min = 67.4,
+                                R.max = 153.1,
+                                FA.angle = NA,
+                                FA.diameter = NA),
+                           class = "rotor")
+    }
+    
+    
+    # Calculate R.avg ----------------------------------------------------------
+    #rotor <- append(rotor, list(R.ave))
 }
