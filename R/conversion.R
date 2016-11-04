3d2c02782f1df452edf95da4679b478eac5ee9a2
wfondrie <wfondrie@umaryland.edu>
2016-11-04 16:00:29 -0400

    added to rcf2rpm

--- R/conversion.R
+++ R/conversion.R
@@ -1,5 +1,13 @@
 rcf2rpm <- function(rcf, rotor) {
 
+  # Check Arguments ------------------------------------------------------------
+  if(!is.numeric(rcf)) stop("rcf argument must be numeric.")
+  if(class(rotor) != "rotor") stop("rotor argument is not a rotor-class object. Use rotor() to create one.")
+
+  # Calculation ----------------------------------------------------------------
+  rpm <- sqrt(rcf / (rotor[[R.ave]] * 11.18)) * 1000
+
+  return(rpm)
 }
 
 rpm2rcf <- function(rpm, rotor) {
