ea06538fc2cc5b6ed8a1090d097ac879ba116627
wfondrie <wfondrie@umaryland.edu>
2016-11-04 20:43:15 -0400

    added some documentation

--- R/conversion.R
+++ R/conversion.R
@@ -1,3 +1,9 @@
+#' RCF Conversion
+#' @description Converts RCF to RPM for a given rotor.
+#' @param rcf relative centrifugal force in x g
+#' @param rotor a rotor object
+#' @return rotor velocity in rotations-per-minute (RPM)
+
 # TODO:
 # - add documentation
 
@@ -12,6 +18,12 @@ rcf2rpm <- function(rcf, rotor) {
 
   return(rpm)
 }
+
+#' RPM Conversion
+#' @description Converts RPM to RCF for given rotor.
+#' @param rpm velocity of the rotor in rotations-per-minute
+#' @param rotor a rotor object
+#' @return relative centrifugal force (RCF) of the rotor in x g
 
 rpm2rcf <- function(rpm, rotor) {
 
